c2945aa8547adf2d076d9ac62665fcaebd405f8b
Brian Claywell <bclaywel@fhcrc.org>
2014-04-11 14:49:14 -0700

    Add some notes and tidy up.

--- shiny-galaxy.R
+++ shiny-galaxy.R
@@ -1,9 +1,18 @@
 #!/usr/bin/env Rscript
 
-require(shiny)
-require(knitr)
+library(shiny)
+library(knitr)
 
 ## THIS IS LITERALLY ALL THAT WE NEED TO DO
-shiny::runApp(system.file("shiny", package="knitr"))
+shiny::runApp(system.file("shiny", package="knitr"), host="0.0.0.0", port=sample(1024:49151, 1))
+
+## TODO add arguments for importing data into R
+##
+##      This might look something like:
+##
+##      $ shiny-galaxy.R --csv hyperfreq.calls:/path/to/hyperfreq_calls.csv --fasta reference.seqs:/path/to/sequences.fasta
+##
+##      Then R you'd have a data frame named hyperfreq.calls and a
+##      $some_library sequence object named reference.seqs available.
 
 ## TODO combine this with grabcpu to fire up a shiny server on gizmo for interactive use
