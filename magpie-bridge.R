3f593999ce6f2754d43dc20fe6aa4ecb08b92e28
Brian Claywell <bclaywel@fhcrc.org>
2014-04-12 13:56:10 -0700

    [WIP] Add Shiny app skeleton and flesh out stubs.

---
+++ magpie-bridge.R
@@ -0,0 +1,26 @@
+#!/usr/bin/env Rscript
+
+## TODO register format handlers somehow
+
+function read.dataset(format, file) {
+    dataset <- NULL
+    ## TODO this should be a lookup in a registry
+    if (format == "csv") {
+        dataset <- read.dataset.csv(file)
+    }
+
+    return(dataset)
+}
+
+function read.dataset.csv(file) {
+    return(read.csv(file, as.is=TRUE))
+}
+
+
+## TODO compile a regex to parse the arguments, FORMAT:R_NAME:FILE
+## ([^:]+):([^:]+):(.+)
+
+args <- commandArgs(trailingOnly=TRUE)
+
+for (s in args) {
+    assign(dataset
--- magpie.R
+++ magpie.R
@@ -3,6 +3,28 @@
 library(shiny)
 library(knitr)
 
+## TODO loop over all the formats we support in some programmatic way
+
+args <- commandArgs(trailingOnly=TRUE)
+
+## TODO actually, I'm confusing myself. This is the *server*, not the Galaxy tool.
+
+for (s in args) {
+    ## arguments are of the form FORMAT:R_NAME:FILE
+
+    ## TODO actually, compile a regex instead of this, it'll be faster and have captures.
+    ## FIXME of course this won't work if there are :s in the file name
+    dataset <- strsplit(dataset_str, ':', fixed=TRUE)
+    ## ? dataset <- c(dataset[1], dataset[2], strjoin(dataset[3:], ':'))
+
+    ## tidy up the variable name to make it valid
+    r.name <- make.names(c(dataset[2]))
+
+    ## TODO invoke an appropriate handler based on FORMAT
+    ## TODO do we always assume no header?
+    assign(r.name, read.csv(file=dataset[3], header=TRUE, as.is=TRUE))
+}
+
 ## THIS IS LITERALLY ALL THAT WE NEED TO DO
 shiny::runApp(system.file("shiny", package="knitr"), host="0.0.0.0", port=sample(1024:49151, 1))
 
