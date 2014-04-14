cbfbac52fe815ce8b28451e626d6c7e8d66e2f6f
Brian Claywell <bclaywel@fhcrc.org>
2014-04-14 15:48:45 -0700

    Tidying.

--- magpie-shiny/ui.R
+++ magpie-shiny/ui.R
@@ -23,7 +23,7 @@ shinyUI(
             ## TODO make the magic happen
             ## TODO eventually this should be packaged, and we can use system.file()
             paste(
-                c(readLines(magpie.payload), '', readLines(file.path('src', 'magpie-demo.Rmd'))),
+                c(readLines(magpie.payload), readLines(file.path('src', 'magpie-demo.Rmd'))),
                 collapse = '\n')
             ),
         tags$textarea(id = 'nbSrc', style = 'display: none;'),
