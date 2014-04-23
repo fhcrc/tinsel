6e6288f976d39bd7eaa9692f2545294a535c6b1c
Brian Claywell <bclaywel@fhcrc.org>
2014-04-22 22:10:43 -0700

    [WIP] Prototyping plugins.

--- magpie/server.R
+++ magpie/server.R
@@ -1,44 +1,34 @@
 library(shiny)
 library(knitr)
 
-shinyServer(function(input, output, session) {
-    cdata <- session$clientData
-
-    knitr.pre <- ""
-    knitr.post <- ""
-
-    observe({
-        query <- parseQueryString(cdata$url_search)
-
-        ## TODO these conditionals could be much more elegant
-
-        src <- ""
-
-        if (!is.null(query$pre)) {
-            knitr.pre <- paste(readLines(query$pre), collapse = "\n")
-        } else {
-            knitr.pre <- ""
-        }
-
-        if (!is.null(query$src)) {
-            src <- paste(readLines(query$src), collapse = "\n")
-        }
-
-        if (!is.null(query$post)) {
-            knitr.post <- paste(readLines(query$post), collapse = "\n")
-        } else {
-            knitr.post <- "\n"
-        }
-
-        updateAceEditor(session, "notebook", value = src)
-    })
-
-    output$knitDocument <- renderUI({
+knitr.plugin <- function(input, output, session) {
+    output$knitrDocument <- renderUI({
         on.exit(unlink("figure/", recursive = TRUE))
 
-        src <- paste(knitr.pre, input$notebook, knitr.post)
+        ## TODO make this unaware of the source plugin
+        src <- input$sourceEditor
 
         if (length(src) == 0L || src == "") return("Nothing to show yet...")
         withMathJax(HTML(paste(knit2html(text = src, fragment.only = TRUE, quiet = TRUE), sep = "\n")))
     })
+}
+
+source.plugin <- function(input, output, session) {
+    cdata <- session$clientData
+
+    observe({
+        query <- parseQueryString(cdata$url_search)
+
+        src <- ""
+        if (!is.null(query$src)) {
+            src <- paste(readLines(query$src), collapse = "\n")
+        }
+
+        updateAceEditor(session, "sourceEditor", value = src)
+    })
+}
+
+shinyServer(function(input, output, session) {
+    source.plugin(input, output, session)
+    knitr.plugin(input, output, session)
 })
--- magpie/ui.R
+++ magpie/ui.R
@@ -1,13 +1,20 @@
 library(shiny)
 library(shinyAce)
 
+knitr.plugin <-
+    list(name = "knitr",
+         output = uiOutput("knitrDocument"))
+
+source.plugin <-
+    list(name = "source",
+         output = list(
+             aceEditor("sourceEditor", mode = "markdown", value = "\n# magpie", height = "640px"),
+             submitButton("knit")))
+
+print(knitr.plugin)
+
 shinyUI(navbarPage(
     img(src = "magpie-64x64.png", style = "height: 1em; vertical-align: top;"),
-    tabPanel("Output",
-             uiOutput("knitDocument")
-             ),
-    tabPanel("Source",
-             aceEditor("notebook", mode = "markdown", value = "## magpie ##\n", height = "640px")
-             ## submitButton("Knit")
-             )
+    tabPanel(knitr.plugin$name, knitr.plugin$output),
+    tabPanel(source.plugin$name, source.plugin$output)
     ))
